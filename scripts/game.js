da09a782966554d0519ec4d7f90c6e96592e3c8a
LucienChotard <chotardlucien@gmail.com>
2019-04-17 00:28:33 +0200

    player animation and more

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,7 @@
 let settings={
   speed:10 ,
   speedMedium:12.5,
-  speedHigh:25,
+  speedHigh:20,
   width:1200,
   height:900,
   margin:150,
@@ -23,8 +23,10 @@ let player={
   jumping:false,
   loose:false,
   score:0,
-  coins:500
+  coins:500,
+  isBestScore:0
 }
+player.isBestScore=localStorage.getItem('bestScore')
 
 // Coins stockage
 let coin=[]
@@ -47,6 +49,8 @@ let loopImgX = [
   [fg]="0"
 ]
 let playerImg = new Image()
+let playerUpImg = new Image
+let playerDownImg = new Image()
 let bgImg = new Image()
 let pipeTopImg = new Image()
 let pipeBottomImg = new Image()
@@ -60,6 +64,8 @@ pipeBottomImg.src = "./images/pipebottom.png"
 sandImg.src = "./images/sand.png"
 scorePlankImg.src = "./images/plank.png"
 coinImg.src = "./images/coin.png"
+playerUpImg.src = "./images/playerUp.png"
+playerDownImg.src = "./images/playerDown.png"
 
 // Canvas setup
 let body = document.querySelector("body")
@@ -81,6 +87,9 @@ let controller={
       break;
       case 40:
         controller.down = keyState
+      break;
+      case 13:
+        replay()
       break;
     }
   }
@@ -108,15 +117,10 @@ function refresh()
 
     // Player limits
     if(player.y>settings.height-settings.margin-20){
-      player.loose=true
-      player.y = settings.height-settings.margin-20
-      player.velY = 0
-
+      loose()
     }
     else if (player.y<0){
-      player.loose=true
-      player.y = 0
-      player.velY = 0
+      loose()
     }
 
     // Obstacles
@@ -131,7 +135,7 @@ function refresh()
         case 10:
           settings.speed = settings.speedMedium
         break;
-        case 20:
+        case 30:
           settings.speed = settings.speedHigh
         break;
       }
@@ -148,35 +152,21 @@ function refresh()
       }
       // Loose conditions
       if (player.x < pipe[i].x + settings.obsWidth &&
-         player.x + player.width > pipe[i].x &&
+         player.x + player.width > pipe[i].x+25 &&
          player.y < pipe[i].y + settings.obsHeight &&
          player.height + player.y > pipe[i].y)
       {
-        player.loose=true
+        loose()
       }
       if (player.x < pipe[i].x + settings.obsWidth &&
-         player.x + player.width > pipe[i].x &&
+         player.x + player.width > pipe[i].x+25 &&
          player.y < pipe[i].y+constant + settings.obsHeight &&
          player.height + player.y > pipe[i].y+constant-20)
       {
-        player.loose=true
+        loose()
       }
 
     }
-
-    // Building player
-    ctx.drawImage(playerImg,player.x,player.y);
-
-    // Building Foreground
-    ctx.drawImage(sandImg,loopImgX[1],settings.height-settings.margin+player.height);
-
-    // Coin Count
-    ctx.drawImage(coinImg,settings.width-100,20)
-    ctx.beginPath()
-    ctx.fillStyle="orange"
-    ctx.font = "60px Arial"
-    ctx.fillText(player.coins, settings.width-200, 80)
-    ctx.closePath
 
     // Loop images
     for(let i =0; i<=loopImgX.length;i++){
@@ -187,15 +177,84 @@ function refresh()
     loopImgX[0]-=1
     loopImgX[1]-=settings.speed
 
-    // Score
+    // Building player
+    if(player.velY>5){
+      ctx.drawImage(playerDownImg,player.x,player.y);
+    }
+    else if (player.velY <= 5 && player.velY >= -5){
+      ctx.drawImage(playerImg,player.x,player.y);
+    }
+    else if (player.velY < -5){
+      ctx.drawImage(playerUpImg,player.x,player.y);
+    }
+
+
+    // Building Foreground
+    ctx.drawImage(sandImg,loopImgX[1],settings.height-settings.margin+player.height);
+
+    // Building Coin Count
+    ctx.drawImage(coinImg,settings.width-100,20)
+    ctx.beginPath()
+    ctx.fillStyle="orange"
+    ctx.font = "60px Arial"
+    ctx.fillText(player.coins, settings.width-200, 80)
+    ctx.closePath
+
+    // Building Score Count
     ctx.drawImage(scorePlankImg,0,0);
     ctx.beginPath()
     ctx.fillStyle="white"
     ctx.font = "60px Arial"
     ctx.fillText(player.score, 50, 70)
-    ctx.closePath
+    ctx.closePath()
 
+    // Building Game over if loose
     if (player.loose){
+        if(player.isBestScore == null || player.isBestScore < player.score){
+          localStorage.setItem("bestScore",player.score)
+        }
+        // Background
+        ctx.beginPath()
+        ctx.fillStyle="black"
+        ctx.fillRect(350,300,500,500)
+        ctx.closePath()
+        // Replay
+        ctx.beginPath()
+        ctx.fillStyle="green"
+        ctx.fillRect(350,600,500,100)
+        ctx.closePath()
+        ctx.beginPath()
+        ctx.fillStyle="white"
+        ctx.font = "35px Arial"
+        ctx.fillText("PRESS ENTER TO REPLAY", 370,660)
+        ctx.closePath()
+        // Best Score
+        ctx.beginPath()
+        ctx.fillStyle="white"
+        ctx.font = "35px Arial"
+        ctx.fillText("BEST SCORE :"+localStorage.getItem('bestScore'), 370,760)
+        ctx.closePath()
+        // Title
+        ctx.beginPath()
+        ctx.fillStyle="white"
+        ctx.font = "60px Arial"
+        ctx.fillText("GAME OVER", 400, 400)
+        ctx.closePath()
+        // Score
+        ctx.drawImage(scorePlankImg,400, settings.height/2+10);
+        ctx.beginPath()
+        ctx.fillStyle="white"
+        ctx.font = "60px Arial"
+        ctx.fillText(player.score, 460, settings.height/2+80)
+        ctx.closePath()
+        // Coins
+        ctx.drawImage(coinImg,700,480)
+        ctx.beginPath()
+        ctx.fillStyle="orange"
+        ctx.font = "60px Arial"
+        ctx.fillText(player.coins,580,540)
+        ctx.closePath
+
         return
     }
     window.requestAnimationFrame(refresh);
@@ -216,6 +275,17 @@ function play(){
   window.addEventListener("keyup", controller.keyListener)
   window.addEventListener("keydown", controller.keyListener)
   window.requestAnimationFrame(refresh);
+
+}
+
+// RePlay
+function replay(){
+  document.location.reload(true);
+}
+
+//loose
+function loose(){
+  player.loose=true
 }
 
 play()
