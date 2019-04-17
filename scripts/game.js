85c75850b51e711e90218191802d09d03069fe96
LucienChotard <chotardlucien@gmail.com>
2019-04-17 13:49:46 +0200

    medusa update, obstacle opti

--- scripts/game.js
+++ scripts/game.js
@@ -11,8 +11,8 @@ let settings={
   obsWidth:180,
   obsHeight:900,
   frame:0,
-  medusaVelY:-3,
-  medusaWidth:31,
+  medusaVelY:-5,
+  medusaWidth:105,
   medusaHeight:60,
 }
 
@@ -49,12 +49,10 @@ medusa[0]={
   y:Math.floor(Math.random() * (0 - 600 + 1)) + 600
 }
 
-// Obstacle stockage
-let tabObs=[]
-let pipe=[]
-pipe[0]={
-  x:settings.width,
-  y:-600,
+let obs=[]
+obs[0]={
+  x:1200,
+  y: Math.floor(Math.random() * (-850 - -350 + 1)) + -350
 }
 
 // Images import
@@ -139,62 +137,19 @@ function refresh()
     player.y+=player.velY
     player.velY*=settings.friction
 
+    // Increasing speed
+    switch(player.score){
+      case 10:
+        settings.speed = settings.speedMedium
+      break;
+    }
+
     // Player limits
     if(player.y>settings.height-settings.margin-20){
       loose()
     }
     else if (player.y<0){
       loose()
-    }
-
-    // Obstacles
-    for(let i=0; i < pipe.length; i++){
-      ctx.drawImage(pipeTopImg,pipe[i].x,pipe[i].y+10);
-      let constant = settings.obsHeight+settings.gap
-      ctx.drawImage(pipeBottomImg,pipe[i].x,pipe[i].y+constant);
-      ctx.drawImage(coinImg,pipe[i].x+50,pipe[i].y+settings.obsHeight+60);
-
-      // Increasing speed
-      switch(player.score){
-        case 10:
-          settings.speed = settings.speedMedium
-        break;
-      }
-      pipe[i].x-=settings.speed
-
-      // New pipe if success
-      if(pipe[i].x == player.x){
-        player.score+=1
-        player.coins+=1
-        localStorage.setItem('coins',player.coins)
-        pipe.unshift({
-           x : settings.width,
-           y : Math.floor(Math.random() * (-850 - -350 + 1)) + -350
-       })
-      }
-      // Loose conditions
-      if (player.x < pipe[i].x + settings.obsWidth &&
-         player.x + player.width > pipe[i].x+25 &&
-         player.y < pipe[i].y + settings.obsHeight &&
-         player.height + player.y > pipe[i].y)
-      {
-        loose()
-      }
-      if (player.x < pipe[i].x + settings.obsWidth &&
-         player.x + player.width > pipe[i].x+25 &&
-         player.y < pipe[i].y+constant + settings.obsHeight &&
-         player.height + player.y > pipe[i].y+constant-20)
-      {
-        loose()
-      }
-      if (player.x < medusa[0].x + settings.medusaWidth &&
-         player.x + player.width > medusa[0].x+25 &&
-         player.y < medusa[0].y + settings.medusaHeight &&
-         player.height + player.y > medusa[0].y-20)
-      {
-        loose()
-      }
-
     }
 
     // Loop images
@@ -217,16 +172,33 @@ function refresh()
       ctx.drawImage(playerUpImg,player.x,player.y);
     }
 
+    // Building Obstacles
+    if(settings.frame%140==0){
+      obs[0].x=settings.width
+      obs[0].y=Math.floor(Math.random() * (-850 - -350 + 1)) + -350
+    }
+    obs[0].x-=settings.speed
+    ctx.drawImage(pipeTopImg,obs[0].x,obs[0].y+10);
+    let constant = settings.obsHeight+settings.gap
+    ctx.drawImage(pipeBottomImg,obs[0].x,obs[0].y+constant);
+    ctx.drawImage(coinImg,obs[0].x+50,obs[0].y+settings.obsHeight+60);
+    if(obs[0].x == player.x){
+      player.score+=1
+      player.coins+=1
+      localStorage.setItem("coins",player.coins)
+    }
 
     // Building medusa
-    if(settings.frame%200==0){
-      medusa[0].x=settings.speed*160
+    if(settings.frame%210==0){
+      medusa[0].x=settings.width
+      if(medusa[0].x > obs[0].x && medusa[0].x < obs[0].x+settings.obsWidth){
+        medusa[0].x= -200
+      }
       medusa[0].y=Math.floor(Math.random() * (0 - 600 + 1)) + 600
     }
     medusa[0].x-=settings.speed
     medusa[0].y-=settings.medusaVelY
     if(medusa[0].y<0 || medusa[0].y > settings.height-settings.margin-20){
-      console.log("bye")
       settings.medusaVelY = -settings.medusaVelY
     }
     ctx.drawImage(medusaImg,medusa[0].x,medusa[0].y);
@@ -249,6 +221,29 @@ function refresh()
     ctx.font = "60px Arial"
     ctx.fillText(player.score, 50, 70)
     ctx.closePath()
+
+    // Loose conditions
+    if (player.x < obs[0].x + settings.obsWidth &&
+       player.x + player.width > obs[0].x+25 &&
+       player.y < obs[0].y + settings.obsHeight &&
+       player.height + player.y > obs[0].y)
+    {
+      loose()
+    }
+    if (player.x < obs[0].x + settings.obsWidth &&
+       player.x + player.width > obs[0].x+25 &&
+       player.y < obs[0].y+constant + settings.obsHeight &&
+       player.height + player.y > obs[0].y+constant-20)
+    {
+      loose()
+    }
+    if (player.x < medusa[0].x + settings.medusaWidth &&
+       player.x + player.width > medusa[0].x+25 &&
+       player.y < medusa[0].y + settings.medusaHeight &&
+       player.height + player.y > medusa[0].y-20)
+    {
+      loose()
+    }
 
     // Building Game over if loose
     if (player.loose){
